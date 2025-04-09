d35b1628b9ac01bcb3e9241e5931bcb89e1fb976
D4nitrix13 <danielperezdev@proton.me>
2025-04-09 06:28:15 -0600

    update

---
+++ .screenlayout/monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
+# Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
+# Correo electrónico: danielperezdev@proton.me
+
+xrandr --output DVI-D-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
