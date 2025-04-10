0560aa11588ff16a7b98e2297479558ed7740635
D4nitrix13 <danielperezdev@proton.me>
2025-04-10 15:00:45 -0600

    update

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/D4nitrix13
+# Gitlab: https://gitlab.com/D4nitrix13
+# Correo electrónico: danielperezdev@proton.me
+
 # systray battery icon
 if [ -d /sys/class/power_supply/BAT0 ]; then
   cbatticon -u 5 &
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,34 +1,70 @@
+"""
+Qtile Configuration File
+
+This file contains the main configuration for the Qtile window manager.
+It sets up the startup script, window settings, key bindings, layouts,
+mouse behavior, and other aspects of the Qtile environment.
+
+Modules:
+- groups: Defines the workspaces for Qtile.
+- keys: Sets up key bindings and modifier keys.
+- layouts: Configures layouts and floating windows.
+- mouse: Configures mouse behavior.
+- path: Contains the path for specific scripts.
+- screens: Defines screen setups and appearance.
+- widgets: Sets up Qtile widgets and their defaults.
+
+Functions:
+- autostart: Runs the autostart.sh script once after Qtile starts.
+"""
+
+
 # Qtile Config File
 # http://www.qtile.org/
 
-# Antonio Sarosi
-# https://www.youtube.com/c/antoniosarosi
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/D4nitrix13
+# Gitlab: https://gitlab.com/D4nitrix13
+# Correo electrónico: danielperezdev@proton.me
+
+from os import path
+from subprocess import call
+from typing import List
 
 from libqtile import hook
 
-from settings.keys import mod, keys
-from settings.groups import groups
-from settings.layouts import layouts, floating_layout
-from settings.widgets import widget_defaults, extension_defaults
-from settings.screens import screens
-from settings.mouse import mouse
+from settings.groups import groups  # noqa: F401
+from settings.keys import keys, mod  # noqa: F401
+from settings.layouts import floating_layout, layouts  # noqa: F401
+from settings.mouse import mouse  # noqa: F401
 from settings.path import qtile_path
-
-from os import path
-import subprocess
+from settings.screens import screens  # noqa: F401
+from settings.widgets import extension_defaults, widget_defaults  # noqa: F401
 
 
 @hook.subscribe.startup_once
-def autostart():
-    subprocess.call([path.join(qtile_path, 'autostart.sh')])
+def autostart() -> None:
+    """
+    Runs the autostart.sh script once after Qtile starts.
+    """
+    call(args=[path.join(qtile_path, "autostart.sh")])
 
 
-main = None
-dgroups_key_binder = None
-dgroups_app_rules = []
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-auto_fullscreen = True
-focus_on_window_activation = 'smart'
-wmname = 'LG3D'
+main: None = None
+dgroups_key_binder: None = None
+dgroups_app_rules: List = []
+follow_mouse_focus: bool = True
+bring_front_click: bool = False
+cursor_warp: bool = False
+auto_fullscreen: bool = True
+focus_on_window_activation: str = "smart"
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
