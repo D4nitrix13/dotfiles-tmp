9a713d4cbec7405db8ce9571b956f5539bfbe556
D4nitrix13 <danielperezdev@proton.me>
2025-04-10 14:04:38 -0600

    update

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -1,7 +1,31 @@
-from typing import Any, List, Tuple
+"""
+This script defines keybindings for various window management, application launching,
+and hardware control in the Qtile window manager.
+
+Keybindings are organized into different categories:
+1. **Window Configurations**: Keybindings for switching windows, resizing windows, toggling floating mode, and changing layouts.
+2. **Application Configurations**: Keybindings for launching common applications like browsers, file explorers, terminals, and menus.
+3. **Hardware Configurations**: Keybindings for controlling volume and brightness using multimedia keys.
+
+Each keybinding is represented by a list containing:
+- Modifiers: A list of modifier keys (e.g., "mod4", "shift").
+- Key: The key to be pressed (e.g., "j", "Tab", "Return").
+- Commands: A list of actions to be executed when the keybinding is triggered.
+
+This script uses `libqtile.config.Key` to bind keys to actions via `lazy` calls, which are delayed executions until the keys are pressed.
+
+Parameters:
+- mod (str): The modifier key (e.g., "mod4" or "mod1").
+- key (Tuple[List[str], str, Any]): A tuple containing a list of modifier keys, the key to press, and the corresponding action(s).
+- keys (List[Key]): A list of Key objects that define all the keybindings.
+
+This configuration is intended to be used with the Qtile window manager to manage windows, applications, and system hardware.
+
+"""
 
 from libqtile.config import Key  # type: ignore
 from libqtile.lazy import lazy  # type: ignore
+from typing import Any, List, Tuple
 
 mod: str = "mod4"
 key: Tuple[List[str], "str", Any]
--- .config/qtile/settings/path.py
+++ .config/qtile/settings/path.py
@@ -1,4 +1,17 @@
+"""
+This script defines the file paths related to the user's Qtile configuration.
+
+It expands the home directory path and constructs the full path to the Qtile configuration
+directory within the user's home directory.
+
+Variables:
+- home (str): The expanded path to the user's home directory.
+- qtile_path (str): The full path to the user's Qtile configuration directory.
+
+This script can be used to reference Qtile configuration files for further automation or customizations.
+"""
+
 from os import path
 
-home = path.expanduser('~')
-qtile_path = path.join(home, ".config", "qtile")
+home: str = path.expanduser(path="~")
+qtile_path: str = path.join(home, ".config", "qtile")
